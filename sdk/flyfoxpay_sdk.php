55c3f056e091c1b2b8b04c4d67d5c6d79b9c51cf
黃柏澄(風雨) <wartw@users.noreply.github.com>
2019-05-22 13:59:16 +0800

    Update flyfoxpay_sdk.php

--- sdk/flyfoxpay_sdk.php
+++ sdk/flyfoxpay_sdk.php
@@ -3,7 +3,7 @@ require_once("flyfoxpay_config/flyfoxpay.config.php");
 function printf_info($data)
 {
     foreach($data as $key=>$value){
-        echo "<font color='#f00;'>";
+                echo "<font color='#f00;'>";
 		if($key=='status'){echo '狀態';}
 		elseif($key=='okpay'){echo '支付成功';}
 		elseif($key=='nopay'){echo '未結帳';}
@@ -27,11 +27,10 @@ function printf_infos($data)
 {
 	$fa=$data['number'];
     for ($x=0; $x<=$fa; $x++) {
-		
-					$dats=$data['list_withdraw'][$x];
-		if($x==$fa){die;}else{
-  foreach($dats as $key=>$value){
-        echo "<font color='#f00;'>";
+		$dats=$data['list_withdraw'][$x];
+    if($x==$fa){die;}else{
+    foreach($dats as $key=>$value){
+                echo "<font color='#f00;'>";
 		if($key=='status'){echo '狀態';}
 		elseif($key=='okpay'){echo '支付成功';}
 		elseif($key=='nopay'){echo '未結帳';}
@@ -49,8 +48,9 @@ function printf_infos($data)
 		elseif($key=='id'){echo '提現ID';}
 		else{echo $key;}
 		echo "</font> : ".$value." <br/>";
-    }	echo '<HR>';
-} }
+}	        echo '<HR>';
+} 
+}
 }
 function curl_post($url,$post)
 {
@@ -64,10 +64,11 @@ $result = curl_exec($ch);
 curl_close ($ch);
 return $result;
 }
+
 $flyfoxpay = new flyfoxpay($flyfoxpay_config);
 class flyfoxpay {
     function __construct($flyfoxpay_config){
-		$this->flyfoxpay_config = $flyfoxpay_config;
+	$this->flyfoxpay_config = $flyfoxpay_config;
 	}
     function flyfoxpay($flyfoxpay_config) {
     	$this->__construct($flyfoxpay_config);
@@ -76,61 +77,64 @@ class flyfoxpay {
      * 建立訂單API
 	 * @type如果未設定將直接選擇全支付方式
      */
-	function addpay($trade_no, $amount, $trade_name, $type) {
-$key=$this->flyfoxpay_config['key'];
-$id=$this->flyfoxpay_config['id'];
-$mail=$this->flyfoxpay_config['mail'];
-$return=$this->flyfoxpay_config['return'];
-$url = "https://sc-i.pw/api/";//API位置
-$post_value=array("key"=>$key, //商家KEY
+function addpay($trade_no, $trade_name, $amount, $type,$customize1,$customize2,$customize3) {
+ $key=$this->flyfoxpay_config['key'];
+ $id=$this->flyfoxpay_config['id'];
+ $mail=$this->flyfoxpay_config['mail'];
+ $return=$this->flyfoxpay_config['return'];
+ $url = "https://sc-i.pw/api/";//API位置
+ $post_value=array(
+       "key"=>$key, //商家KEY
        "id"=>$id, //商家ID
        "mail"=>$mail, //商家EMAIL
        "trade_no"=>$trade_no, //商家訂單ID
        "amount"=>$amount, //訂單金額(需大於50)
        "trade_name"=>$trade_name, //訂單名稱
-	   "type"=>$type, //指定付款方式，預設為all
+       "type"=>$type, //指定付款方式，預設為all
        "return"=>$return, //支付完成返回網址
-      ); 
-$output = curl_post($url,$post_value);
-$json=json_decode($output, true);
+       "customize1"=>$customize1,//自訂義1
+       "customize2"=>$customize2,//自訂義2
+       "customize3"=>$customize3,//自訂義3
+                    ); 
+ $output = curl_post($url,$post_value);
+ $json=json_decode($output, true);
 
 return array('status' => $json['status'], 'url' => $json['url'], 'error' => $json['error']);
 }
 /**
      * 檢查訂單API
      */
-function check($trade_no) {
-$key=$this->flyfoxpay_config['key'];
-$id=$this->flyfoxpay_config['id'];
-$mail=$this->flyfoxpay_config['mail'];
-$url = "https://sc-i.pw/api/check/";//API位置
-$post_value= array("key"=>$key, //商家KEY
+function check($trade_no,$trade_nos) {
+ $key=$this->flyfoxpay_config['key'];
+ $id=$this->flyfoxpay_config['id'];
+ $mail=$this->flyfoxpay_config['mail'];
+ $url = "https://sc-i.pw/api/check/";//API位置
+ $post_value= array(
+       "key"=>$key, //商家KEY
        "id"=>$id, //商家ID
        "mail"=>$mail, //商家EMAIL
        "trade_no"=>$trade_no, //商家訂單ID
-       ); 
-$output = curl_post($url,$post_value);
+       "trade_nos"=>$trade_nos, //支付號ID
+                    ); 
+   $output = curl_post($url,$post_value);
+   $json=json_decode($output, true);
+   $security1  = array();
 
-$security1  = array();
+   $security1['mchid']      = $id;//商家ID
 
-$security1['mchid']      = $id;//商家ID
+   $security1['status']        = "7";//驗證，請勿更改
 
-$security1['status']        = "7";//驗證，請勿更改
+   $security1['mail']      = $mail;//商家EMAIL
 
-$security1['mail']      = $mail;//商家EMAIL
-
-$security1['trade_no']      = $trade_no;//商家訂單ID
-
-foreach ($security1 as $k=>$v)
-
-{
-
+   $security1['trade_no']      = $json['trade_no'];//商家訂單ID
+	
+   $o='';
+     foreach ($security1 as $k=>$v)
+  {
     $o.= "$k=".($v)."&";
+  }
 
-}
-
-$sign1 = md5(substr($o,0,-1).$key);//**********請替換成商家KEY
-$json=json_decode($output, true);
+    $sign1 = md5(substr($o,0,-1).$key);//**********請替換成商家KEY
 if($json['sign']==$sign1){
   $sHtml = array('status' => $json['status'],'status_trade' => $json['status_trade'],'msg' => '驗證成功','type'=>$json['type'],'trade_no'=>$json['trade_no']);
 }else{
@@ -226,7 +230,7 @@ $url = "https://sc-i.pw/api/withdraw/";//API位置
 $post_value= array("key"=>$key, //商家KEY
        "id"=>$id, //商家ID
        "mail"=>$mail, //商家EMAIL
-	   "money"=>$money, //提現金額(依照"費率表-提現手續費"中為準)
+       "money"=>$money, //提現金額(依照"費率表-提現手續費"中為準)
        "type"=>$type, //提現方式(這裡以支付寶為例)
        "alipay"=>$bank, //支付寶帳號
        "alipay_name"=>$bank_name, //支付寶帳號所有人名字
@@ -295,7 +299,7 @@ $post_value= array("key"=>$key, //商家KEY
        "id"=>$id, //商家ID
        "mail"=>$mail, //商家EMAIL
        //二選一，同時存在已null優先
-	   "null"=>$notify_url, //清除現有notify_url(請固定填入1)
+       "null"=>$notify_url, //清除現有notify_url(請固定填入1)
        "notify_url"=>$notify_url, //callback網址(https://申請時的網址/路徑)
        ); 
 $output = curl_post($url,$post_value);
