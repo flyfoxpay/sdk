41876912b85aaffbad055fbf64fb21bf6b41e751
黃柏澄(風雨) <wartw@users.noreply.github.com>
2019-05-22 13:50:19 +0800

    Create flyfoxpay.config.php

---
+++ sdk/flyfoxpay_config/flyfoxpay.config.php
@@ -0,0 +1,16 @@
+<?php
+/* *
+ * 設定文件
+ */
+ 
+//↓↓↓↓↓↓↓↓↓↓請在這裡設置↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+//商家KEY
+$flyfoxpay_config['key']		= '6f7f52f5cc84389fdb66115acf226ff9ce64662d';
+//商户ID
+$flyfoxpay_config['id']		= '6180470071';
+//商户EMAIL
+$flyfoxpay_config['mail']			= 'wartw98@gmail.com';
+//訂單返回網址(如果沒有要返回請留空)
+$flyfoxpay_config['return']='';
+
+?>
